fe8ebf24102ed8f5c18e465d957eb4f9f290e192
Ash <ashlandmwest@gmail.com>
2021-09-14 10:32:06 -0700

    Seporate Menu to its own component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NavHeader from "./components/NavHeader";
 import Post from "./components/Post";
 import postData from "./data/postData";
 import Footer from "./components/Footer";
+import Menu from "./components/Menu";
 
 function App() {
   let posts = postData.posts.map((post) => <Post {...post} />);
@@ -14,55 +15,7 @@ function App() {
       <NavHeader />
 
       {/* <!-- Menu --> */}
-      <section id="menu">
-        {/* <!-- Search --> */}
-        <section>
-          <form class="search" method="get" action="#">
-            <input type="text" name="query" placeholder="Search" />
-          </form>
-        </section>
-
-        {/* <!-- Links --> */}
-        <section>
-          <ul class="links">
-            <li>
-              <a href="#">
-                <h3>Lorem ipsum</h3>
-                <p>Feugiat tempus veroeros dolor</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <h3>Dolor sit amet</h3>
-                <p>Sed vitae justo condimentum</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <h3>Feugiat veroeros</h3>
-                <p>Phasellus sed ultricies mi congue</p>
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <h3>Etiam sed consequat</h3>
-                <p>Porta lectus amet ultricies</p>
-              </a>
-            </li>
-          </ul>
-        </section>
-
-        {/* <!-- Actions --> */}
-        <section>
-          <ul class="actions stacked">
-            <li>
-              <a href="#" class="button large fit">
-                Log In
-              </a>
-            </li>
-          </ul>
-        </section>
-      </section>
+      <Menu />
 
       {/* <!-- Main --> */}
       <div id="main">
